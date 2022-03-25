7e5486c0a20f8a78062e87321bf782b42df4a4ed
Malek Farag <faragmalek14@gmail.com>
2022-03-25 12:06:39 -0400

    added mailchimp form

--- templates/footer.php
+++ templates/footer.php
@@ -25,17 +25,47 @@
     </nav>
 
     <div class="stack mail_list_social">
-        <form class="mail_list_div">
+        <!-- <form class="mail_list_div">
             <h5 class="subhead">Join Our Mailing List!</h5>
             <div class="mailing_inputs">
                 <input type="email" name="email" id="email_id" placeholder="Email...">
                 <button class="btn btn_primary" type='submit'>Join</button>
             </div>
+        </form> -->
+
+        <!-- Begin Mailchimp Signup Form -->
+        <form action="https://polarhr.us14.list-manage.com/subscribe/post?u=758e25ff02b554ad68b70d289&amp;id=b8a47ef406" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate mail_list_div" target="_blank" novalidate>
+
+
+            <h5 class="subhead">Join Our Mailing List!</h5>
+            <div class="mailing_inputs">
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email...">
+                <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn_primary">Join</button>
+            </div>
+
+            
+
+            <div id="mce-responses" class="clear foot" style="display:none">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    
+
+
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_758e25ff02b554ad68b70d289_b8a47ef406" tabindex="-1" value=""></div>
+                <div class="optionalParent">
+                    <div class="clear foot">
+                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button hidden">
+                    </div>
+                </div>
+            </div>
         </form>
+        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+        <!--End mc_embed_signup-->
 
         <div class="social_nav">
-            <a href="" class="item fb img"></a>
-            <a href="" class="item linkedin img"></a>
+            <a href="#" class="item fb img"></a>
+            <a href="#" class="item linkedin img"></a>
         </div>
     </div>
     
